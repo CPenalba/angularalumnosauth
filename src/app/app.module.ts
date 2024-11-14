d9860ae6c998867844070a5ef759cd14abb3666a
CPenalba <carolinapenalbacorpa@gmail.com>
2024-11-14 19:37:17 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/login/login.component';
+import { CrearAlumnoComponent } from './components/crear-alumno/crear-alumno.component';
+import { ModificarAlumnoComponent } from './components/modificar-alumno/modificar-alumno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    CrearAlumnoComponent,
+    ModificarAlumnoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/modificar-alumno/modificar-alumno.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-alumno',
+  templateUrl: './modificar-alumno.component.html',
+  styleUrl: './modificar-alumno.component.css'
+})
+export class ModificarAlumnoComponent {
+
+}
